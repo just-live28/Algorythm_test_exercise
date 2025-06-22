c04df3e42643986905dca6b8a4087738f7fdadc7
just-live28 <jkhkorea28@naver.com>
2025-06-22 23:33:11 +0900

    250622

---
+++ backjoon/class1/2588.py
@@ -0,0 +1,11 @@
+num1 = int(input())
+num2 = int(input())
+
+# print(num1 * (num2 % 10))
+# print(num1 * (num2 // 10))
+# print(num1 * (num2 // 100))
+
+
+print(num2 % 10)
+print((num2 // 10) % 10)
+print(num2 // 100)
